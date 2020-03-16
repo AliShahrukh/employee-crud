134eefd33cd06780f4c61aa0096ad0b6e813c7c2
Shahrukh <37803285+AliShahrukh@users.noreply.github.com>
2020-03-16 15:27:11 +0530

    initial commit

---
+++ src/main/java/com/ali/springboot/jpacrud/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.ali.springboot.jpacrud.repository;
+
+import com.ali.springboot.jpacrud.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/test/java/com/ali/springboot/jpacrud/JpacrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.springboot.jpacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpacrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
